43ff9d5d578f7b7bcfb4faf0771f8155166e6a48
dhtdht020 <dhtdht020@gmail.com>
2020-10-06 17:00:18 +0300

    Initial commit

---
+++ api.py
@@ -0,0 +1,110 @@
+import urllib.request
+import flask
+import parselist
+import requests
+import yaml
+from apscheduler.schedulers.background import BackgroundScheduler
+
+app = flask.Flask(__name__)
+# app.config["DEBUG"] = True
+app.config["JSONIFY_PRETTYPRINT_REGULAR"] = True
+
+
+# All Packages:
+# /v1/<host>/packages
+#
+# All Packages in category:
+# /v1/<host>/packages/category/<category>
+#
+# Specific Package:
+# /v1/<host>/package/<name>
+#
+# Hosts:
+# /v1/hosts
+
+
+# Get host list
+def get_hosts():
+    global parsed_hosts
+    hosts_file = requests.get("https://raw.githubusercontent.com/dhtdht020/oscdl-updateserver/master/v1/announcement"
+                                     "/repositories.yml").text
+    parsed_hosts = yaml.load(hosts_file, Loader=yaml.FullLoader)
+    print("Loaded hosts list")
+
+
+get_hosts()
+
+
+# Schedule hosts list for refresh once per 30 minutes
+scheduler = BackgroundScheduler()
+scheduler.add_job(func=get_hosts, trigger="interval", minutes=30)
+scheduler.start()
+
+
+def get_hostname(host):
+    return parsed_hosts["repositories"][host]["host"]
+
+
+def url(host):
+    opener = urllib.request.build_opener()
+    opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+    urllib.request.install_opener(opener)
+
+    try:
+        test_list_url = f"https://{get_hostname(host)}/hbb/homebrew_browser/listv036.txt"
+        return test_list_url
+    except KeyError:
+        test_list_url = f"https://hbb1.oscwii.org/hbb/homebrew_browser/listv036.txt"
+        return test_list_url
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return "<h1>Error 404: Opened the shop but there were no apps." \
+           "</h1><p>That doesn't look like our API. Did you follow the documentation?</p>", 404
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>Open Shop Channel API</h1><p>Welcome to the OSC API Public Beta! " \
+           "For documentation, go to <documentation></p>"
+
+
+@app.route('/v1/hosts', methods=['GET'], strict_slashes=False)
+def api_hosts():
+    return flask.jsonify(parsed_hosts)
+
+
+@app.route('/v1/<host>/packages', methods=['GET'], strict_slashes=False)
+def all_packages(host):
+    test_list_url = url(host)
+
+    f, headers = urllib.request.urlretrieve(test_list_url)
+    l = parselist.convert_list_file_to_json(f)
+    parser = parselist.hbbjsonparser()
+    parser.load_json(l)
+    return flask.jsonify(l)
+
+
+@app.route('/v1/<host>/package/<app_name>', methods=['GET'], strict_slashes=False)
+def one_package(host, app_name):
+    test_list_url = url(host)
+
+    f, headers = urllib.request.urlretrieve(test_list_url)
+    l = parselist.convert_list_file_to_json(f)
+    parser = parselist.hbbjsonparser()
+    parser.load_json(l)
+    return flask.jsonify(parser.dictionary(app_name))
+
+
+@app.route('/v1/<host>/category/<category>/packages', methods=['GET'], strict_slashes=False)
+def category_packages(host, category):
+    test_list_url = url(host)
+
+    f, headers = urllib.request.urlretrieve(test_list_url)
+    l = parselist.convert_list_file_to_json(f)
+    parser = parselist.hbbjsonparser()
+    parser.load_json(l)
+    return flask.jsonify(parser.get_category(category))
+
+
+app.run()
---
+++ parselist.py
@@ -0,0 +1,194 @@
+# LyfeOnEdge 2020
+# Written for Open Shop Channel Project
+
+import io, json
+
+
+# python object to parse converted hbb list file
+class hbbjsonparser(object):
+    def __init__(self):
+        self.init()
+
+    def init(self):
+        self.all = []
+        self.demos = []
+        self.emulators = []
+        self.games = []
+        self.media = []
+        self.utilities = []
+
+        self.map = {
+            "demos": self.demos,
+            "emulators": self.emulators,
+            "games": self.games,
+            "media": self.media,
+            "utilities": self.utilities,
+        }
+
+        self.list_list = [self.all, self.demos, self.emulators, self.games, self.media, self.utilities]
+
+    def clear(self):
+        self.init()
+
+    # Loads appstore json as a large list of dicts
+    def load_json(self, repo_json):
+        if not repo_json:
+            raise
+        self.clear()
+        self.all = repo_json
+        self.sort()
+        num_entries = len(self.all)
+        print(f"Loaded {num_entries} packages")
+
+    def get_package_dict(self, packagename: str):
+        for package in self.all:
+            if package["internal_name"] == packagename:
+                return package
+
+    def get_category(self, category):
+        packages = []
+        for package in self.all:
+            if package["category"] == category:
+                packages.append(package)
+
+        return packages
+
+    # sorts list into smaller chunks
+    def sort(self):
+        if self.all:
+            for entry in self.all:
+                try:
+                    self.map[entry["category"]].append(entry)
+                except:
+                    pkg = entry["internal_name"]
+                    print(f"Error sorting {pkg}")
+        else:
+            raise
+
+    def list(self):
+        packages = []
+        for p in self.all:
+            packages.append(p["internal_name"])
+        return packages
+
+    def dictionary(self, app_name):
+        return self.get_package_dict(app_name)
+
+
+def convert_list_file_to_json(list_file):
+    with open(list_file) as f:
+        contents = io.StringIO(f.read())
+    return convert_list_to_json(contents)
+
+
+def convert_list_to_json(hbblist):
+    def repo_entry():
+        entry = {
+            "internal_name": "",  # String
+            "display_name": "",  # String
+            "coder": "",  # String
+            "version": "",  # String
+            "short_description": "",  # String
+            "long_description": "",  # String
+            "release_date": "",  # String
+            "contributors": "",  # String
+            "updated": "",  # timestamp int
+            "category": "",  # String
+            "package_type": "",
+            "extracted": 0,  # Extracted size, int
+            "zip_size": 0,  # Zip size, int
+            "downloads": 0,  # dl's, int
+            "controllers": "",
+            # "folders_to_create": ".",
+            "rating": "",
+        }
+        return entry
+
+    # First line is header, 'Homebrew' expected as first item
+    header = hbblist.readline().split()
+
+    if not header[0] == "Homebrew":
+        raise valueError("Invalid Repo File")
+
+    # Not documented, small int
+    unknown = header[1]
+
+    repo_version = header[2]
+    if len(header) > 3:
+        changenotes = header[2:]
+    else:
+        changenotes = []
+
+    # Category changes as we cycle through homebrew
+    TRANSITIONS = {
+        "=Demos=": "emulators",
+        "=Emulators=": "games",
+        "=Games=": "media",
+        "=Media=": "utilities",
+        "=Utilities=": None,
+    }
+    current_category = "demos"
+
+    packages = []
+    while True:
+        line = hbblist.readline()
+        if line.strip().strip("\\n") in TRANSITIONS:  # Check if the line is a footer line
+            current_category = TRANSITIONS[line.strip().strip("\\n")]  # If so, update category
+            if not current_category:  # Stop if there are no more categories to read
+                break
+            continue  # Skip processing line if it's a transition
+
+        line = line.split()
+        name = line[0]
+        updated = int(line[1])
+        unknown = line[2]
+        boot_file_size = line[3]
+        package_type = line[4]
+        folder_size = line[5]
+        downloads = line[6]
+        rating = line[7]
+        controllers = line[8]
+        # folders_to_create = line[9].replace(";", " ").split()
+        display_name = hbblist.readline().strip().strip("\\n")
+        author = hbblist.readline().strip().strip("\\n")
+        version = hbblist.readline().strip().strip("\\n")
+        extracted = hbblist.readline().strip().strip("\\n")
+        description = hbblist.readline().strip().strip("\\n")
+        details = hbblist.readline().strip().strip("\\n")
+
+        entry = repo_entry()
+        entry["internal_name"] = name
+        entry["display_name"] = display_name
+        entry["coder"] = author
+        entry["version"] = version
+        entry["short_description"] = description
+        entry["long_description"] = details
+        entry["release_date"] = updated
+        entry["category"] = current_category
+        entry["package_type"] = package_type
+        entry["extracted"] = int(extracted)
+        entry["updated"] = updated  # str(date.strftime(updated, "%m/%d/%Y"))
+        entry["zip_size"] = int(folder_size)
+        entry["downloads"] = int(downloads)
+        # entry["folders_to_create"] = folders_to_create
+        entry["controllers"] = controllers
+
+        # Collect all generated libget-style repo entries from the list
+        packages.append(entry)
+
+    return packages
+
+
+if __name__ == "__main__":
+    import urllib.request
+
+    opener = urllib.request.build_opener()
+    opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+    urllib.request.install_opener(opener)
+
+    test_list_url = "https://hbb1.oscwii.org/hbb/homebrew_browser/listv036.txt"
+
+    f, headers = urllib.request.urlretrieve(test_list_url)
+    l = convert_list_file_to_json(f)
+    parser = hbbjsonparser()
+    parser.load_json(l)
